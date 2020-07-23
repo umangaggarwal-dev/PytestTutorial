32c5d4152d7be67ef5254e65d9419b4ad3644eb9
um.aggarwal <umang.aggarwal@geminisolutions.in>
2020-07-23 11:21:24 +0530

    Text for chapter 2

--- test/working_with_mock/test_case_6_mock.py
+++ test/working_with_mock/test_case_6_mock.py
@@ -6,6 +6,20 @@ from tester.classes.db_connection import DbConnection
 from tester.classes.foo import Foo
 
 
+'''
+Python's unittest library has the module mock, which was added to it after python 3.3,
+for earlier versions we have to install this module with - pip install mock
+
+Function Mock of module mock creates a mock instance of any class,
+which is specified with the "spec" parameter, although it does not implement any of the functions.
+
+We can define the return value of any function or property of the mock instance,
+by defining a value of mock_instance.<function or property>.return_value
+
+We are using a module of unittest, because pytest is written as enhancement over unittest.
+Infact patching which we used earlier is also a part of unittest library,
+which we used using mocker fixture of pytest, but it actually utilizes modules of unittest.
+'''
 @pytest.fixture(name="mock_db")
 def get_mock_db():
     mock_db = Mock(spec=DbConnection)
--- test/working_with_mock/test_case_7_side_effect.py
+++ test/working_with_mock/test_case_7_side_effect.py
@@ -5,6 +5,13 @@ from tester.classes.foo import Foo
 from tester.classes.service import Service
 
 
+'''
+Since Mock does not implement any function or property of the class it copies,
+we can define a particular property using a side effect, 
+this is different from giving a certain return value, 
+because a side_effect is executed when the property of the mock instance is called,
+and returns values depending on the input/output of the side effect stub definition. 
+'''
 #side effect stub
 @pytest.fixture(name="process_data_stub")
 def get_process_data_stub():
