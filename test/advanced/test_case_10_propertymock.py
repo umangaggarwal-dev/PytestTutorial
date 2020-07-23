33e404c3c5aca15e8060bc19e996cafc78cc8bc8
um.aggarwal <umang.aggarwal@geminisolutions.in>
2020-07-23 11:44:28 +0530

    Text for chapter 3

--- test/advanced/test_case_10_propertymock.py
+++ test/advanced/test_case_10_propertymock.py
@@ -1,10 +1,18 @@
 from tester.classes.stack import Stack
 
 
+'''
+A property mock is used to replace a certain property (variable or function)
+of a class, it comes handy when we have to write test cases which utilizes user defined classes.
+
+The first case simply patches function get_data of class Stack.
+
+The second case binds a PropertyMock object to a MagicMock instance of class Stack. 
+'''
 def test_use_propertymock_on_stack(mocker):
     patch = mocker.patch.object(Stack, "get_data", new_callable=mocker.PropertyMock)
     patch.return_value = "blah"
-    stack = Stack([1,2,3])
+    stack = Stack([1, 2, 3])
     assert stack.get_data == "blah"
 
 
--- test/advanced/test_case_8_monkeypatch.py
+++ test/advanced/test_case_8_monkeypatch.py
@@ -5,10 +5,20 @@ import pytest
 from tester.pure_functions.case_5_stubs import run_subprocess
 
 
+'''
+Monkeypatch is an alternate to unittest patch, and is native to pytest.
+We should understand that there is no added advantage of monkeypatch over a normal mocker.patch
+infact monkeypatch being a fixture has the inherent risks of a fixture,
+but sometimes monkeypatch can reduce the code to some extent.
+
+Here we are revisiting the code of test case 5,
+look at the differences, 
+we are no longer creating a mock class manually since we already know how to use Mock 
+'''
 @pytest.fixture(name="mock_popen")
 def get_mock_popen(mocker):
     mock_popen = mocker.Mock(spec=subprocess.Popen)
-    mock_popen.communicate.return_value = ("blah","")
+    mock_popen.communicate.return_value = ("blah", "")
     return mock_popen
 
 
